79d5acd82bf38568f1c386d1a18d601192d0e6e9
xCarlok13 <carlos.badiasanchis@gmail.com>
2025-03-25 09:02:05 +0100

    Implementación restar y multiplicar

--- CalculadoraGitEclipse/calculadoraGitEclipse/Calculadora.java
+++ CalculadoraGitEclipse/calculadoraGitEclipse/Calculadora.java
@@ -5,6 +5,14 @@ public class Calculadora {
 		return a + b;
 	}
 
+	public int restar(int a, int b) {
+		return a - b;
+	}
+
+	public int multiplicar(int a, int b) {
+		return a * b;
+	}
+
 	public static void main(String[] args) {
 		Calculadora calc = new Calculadora();
 		System.out.println("Resultado de la suma: " + calc.sumar(5, 3));
